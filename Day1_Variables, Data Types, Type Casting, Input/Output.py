6c429eeff67037f7abba777841e918397cecf261
Abdul Rafay <abdulrafayofficial038@gmail.com>
2026-05-13 18:41:17 +0500

    Day 1 completed

---
+++ Day1_Variables, Data Types, Type Casting, Input/Output.py
@@ -0,0 +1,7 @@
+name = input("Enter Your Name: ")
+height = int(input("Enter Your Height in Inches: "))
+
+height_in_feet = height / 12
+
+print(f"Your name is {name} and your height is {height} inches.")
+print(f"{name}, your height in feet is {height_in_feet : .2f}")
