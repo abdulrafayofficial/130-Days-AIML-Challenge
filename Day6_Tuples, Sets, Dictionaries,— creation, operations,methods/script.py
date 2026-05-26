1158e630471d4af4344b099549af7fb26ec209cc
Abdul Rafay <abdulrafayofficial038@gmail.com>
2026-05-26 22:38:59 +0500

    Day6_Tuples, Sets, Dictionaries,— creation, operations,methods

---
+++ Day6_Tuples, Sets, Dictionaries,— creation, operations,methods/script.py
@@ -0,0 +1,61 @@
+# Tuple
+# Tuples are used to store multiple items in a single variable.
+# Tuple is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Set, and Dictionary, all with different qualities and usage.
+# A tuple is a collection which is ordered and unchangeable.
+# Tuples are written with round brackets.
+
+mytuple = ("apple", "banana", "cherry")
+print(mytuple)
+
+#⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘
+#⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘
+
+# Ordered
+# When we say that tuples are ordered, it means that the items have a defined order, and that order will not change.
+
+# Unchangeable
+# Tuples are unchangeable, meaning that we cannot change, add or remove items after the tuple has been created.
+
+# Allow Duplicates
+# Since tuples are indexed, they can have items with the same value:
+
+
+#⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘
+#⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘
+# Create Tuple With One Item
+thistuple = ("apple",) # we have to put a comma afterward to represent it is a tuple!!
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+
+
+
+#⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘
+#⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘
+# Access Tuple Items
+# You can access tuple items by referring to the index number, inside square brackets:
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[1])
+print(thistuple[2:5])
+print(thistuple[:4])
+print(thistuple[2:])
+print(thistuple[-4:-1])
+
+
+#⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘
+#⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘
+# Python - Update Tuples
+
+# Once a tuple is created, you cannot change its values. Tuples are unchangeable, or immutable as it also is called.
+# But there is a workaround. You can convert the tuple into a list, change the list, and convert the list back into a tuple.
+
+x = ("apple","banana","cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+print(x)
+
+
