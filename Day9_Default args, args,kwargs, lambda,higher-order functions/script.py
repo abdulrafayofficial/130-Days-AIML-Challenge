591727cf6b8a74293e66d91dea991168f3f0e5fc
Abdul Rafay <abdulrafayofficial038@gmail.com>
2026-07-01 13:48:02 +0500

    Day 9 Coompleted_Default args, args,kwargs, lambda,higher-order functions

--- Day9_Default args, args,kwargs, lambda,higher-order functions/script.py
+++ Day9_Default args, args,kwargs, lambda,higher-order functions/script.py
@@ -157,11 +157,10 @@ hello(quiet)
 #----------------------------
 
 def divisor(x):
-    def divident(y):
-        return y/x
-    return divident
+    def dividend(y):  
+        return y / x
+    return dividend
 
 divide = divisor(2)
 print(divide(10))
-
 
